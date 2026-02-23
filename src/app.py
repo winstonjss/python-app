268a94407110b1e4cc8713e4ffb460cbe207c087
Winston Sotelo <jhonatan_15-01@hotmail.com>
2026-02-22 21:25:47 -0600

    Adds ci git hub worflo

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hola mundo :)", 200
+    return "Hola mundo", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
