36c526660924082939c14f68669a5ecb1744d155
Winston Sotelo <jhonatan_15-01@hotmail.com>
2026-02-25 14:29:48 -0600

    fixed, adjist

--- src/app.py
+++ src/app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify
+import datetime
+import socket
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hola mundo :))", 200
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/api/v1/info')
+
+def info():
+    return jsonify({
+    	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
+    	'hostname': socket.gethostname(),
+        'message': 'You are doing great, little human! <3',
+        'deployed_on': 'kubernetes'
+    })
+
+@app.route('/api/v1/healthz')
+
+def health():
+	# Do an actual check here
+    return jsonify({'status': 'up'}), 200
+
+if __name__ == '__main__':
+
+    app.run(host="0.0.0.0")
