41ef20b8d26c2b272d1c8036ebda7c16debaaa52
Winston Sotelo <jhonatan_15-01@hotmail.com>
2026-03-15 19:39:44 -0500

    Added, add step installation argocd

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def details():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, little humans 4'
+        'message': 'You are doing great, little humans 5'
     })
 
 @app.route('/api/v1/healthz')
