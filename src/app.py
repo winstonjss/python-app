960e45b608b6be849fb3ca8796dc5309d6748cb9
Winston Sotelo <jhonatan_15-01@hotmail.com>
2026-03-15 19:13:23 -0500

    Added, add cicd jobs

--- src/app.py
+++ src/app.py
@@ -11,7 +11,8 @@ app = Flask(__name__)
 def details():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
-    	'hostname': socket.gethostname()
+    	'hostname': socket.gethostname(),
+        'message': 'You are doing great, little humans'
     })
 
 @app.route('/api/v1/healthz')
