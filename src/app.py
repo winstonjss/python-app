292eab721818afaa8706bf91a16e0972c0d4272f
Winston Sotelo <jhonatan_15-01@hotmail.com>
2026-03-28 19:33:24 -0500

    fixed, adjust step ci

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def details():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, little humans 11'
+        'message': 'You are doing great, little humans 13'
     })
 
 @app.route('/api/v1/healthz')
